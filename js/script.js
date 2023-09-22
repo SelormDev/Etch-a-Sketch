d8489dad29fad6359cbbe65b12b1d15193613e67
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-09-22 00:10:19 +0000

    Implemented grid size generation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const colorValue = colorPicker.value;
 const raindowBtn = document.querySelector("input[value=Rainbow]");
 const eraserBtn = document.querySelector("input[value=Eraser]");
 const clearBtn = document.querySelector("input[value=Clear]");
+const sizeBtn = document.querySelector("input[value=Size]");
 
 const gridContainer = document.querySelector(".grid-container");
 const grid = document.querySelector(".grid");
@@ -16,18 +17,31 @@ gridColumn.style.display = "flex";
 
 grid.append(gridColumn);
 
+let size;
+function gridSize() {
+  size = prompt("Enter grid size");
+  while (size > 100) {
+    size = prompt("Highest grid size is 100!");
+  }
+}
+
+gridSize();
+
+let divHeight = 550 / size;
+let divWidth = 1000 / size;
+
 const div = document.createElement("div");
 div.style.backgroundColor = "#72a4f0";
-div.style.border = "0.1px solid black";
-div.style.width = "20px";
-div.style.height = "11px";
+div.style.border = `2px solid black`;
+div.style.width = `${divWidth}px`;
+div.style.height = `${divHeight}px`;
 
 function createGridSize() {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < size; i++) {
     gridRow.append(div.cloneNode(true));
   }
 
-  for (let j = 0; j < 49; j++) {
+  for (let j = 0; j < size - 1; j++) {
     gridColumn.appendChild(gridRow.cloneNode(true));
   }
 }
